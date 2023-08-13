373dba8902129ea3570f3e216ea9c6d5a39b7d1c
ayaMohy <aya.mohey162@gmail.com>
2023-08-13 11:09:16 +0300

    adding serch input and handle error

--- src/components/weather-data.vue
+++ src/components/weather-data.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="full-width weather q-ma-auto" v-if="weatherInfo">
+    <div class="weather__input container">
+      <q-input
+        standout=" text-lightBlue"
+        v-model="cityFeild"
+        label="Enter City Name"
+        outlined
+        @keyup.enter="submit"
+      />
+    </div>
     <div class="weather__now q-ma-auto flex justify-center items-center">
       <div class="q-pr-md weather__now-text" style="margin: 0">
         {{ weatherInfo.name }}
@@ -20,7 +29,7 @@
     >
       <div>{{ tempDegree }}&deg; {{ temp_unit }}</div>
       <q-btn
-        :label="temp_unit == 'C' ? 'Fahrenheit': 'Celsius '"
+        :label="temp_unit == 'C' ? 'Fahrenheit' : 'Celsius '"
         @click="toggleTemp"
         class="weather__degree--btn-toggle"
       />
@@ -31,40 +40,49 @@
 <script setup>
 import axios from "axios";
 import { onBeforeMount, ref } from "vue";
-
+import { useQuasar } from "quasar";
+const $q = useQuasar();
 const weatherInfo = ref(null);
 const tempDegree = ref(null);
 const temp_unit = ref("C");
-const city = "New Cairo";
+const cityFeild = ref("New Cairo");
 const getCurentWeather = async () => {
   try {
     const { data } = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=078be31b22e6ece778194ba4d21762af`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityFeild.value}&limit=1&appid=078be31b22e6ece778194ba4d21762af`
     );
     const response = await axios.get(`
-https://api.openweathermap.org/data/2.5/weather?lat=${data[0]?.lat}&lon=${data[0].lon}&units=metric&appid=078be31b22e6ece778194ba4d21762af
+https://api.openweathermap.org/data/2.5/weather?lat=${data[0]?.lat}&lon=${data[0]?.lon}&units=metric&appid=078be31b22e6ece778194ba4d21762af
 `);
     weatherInfo.value = await response.data;
     tempDegree.value = await weatherInfo.value.main.temp;
-  } catch (error) {
-    console.log(error);
   }
-  console.log(weatherInfo.value);
+  // handle error
+  catch (error) {
+    $q.dialog({
+      title: "Error",
+      message:
+        "Unable to establish a network connection or incorrect city name entered. Please check your internet connection and ensure the city name is spelled correctly, then try again.",
+    })
+    .onOk(() => {
+      cityFeild.value=''
+    })
+  }
 };
 
 // toggle between temperature
 const toggleTemp = () => {
   if (temp_unit.value == "C") {
-    console.log("f:", tempDegree.value *9);
     tempDegree.value = (tempDegree.value * 9) / 5 + 32;
     temp_unit.value = "F";
   } else {
-    console.log("c:", tempDegree.value- 32 );
     tempDegree.value = ((tempDegree.value - 32) * 5) / 9;
     temp_unit.value = "C";
   }
 };
-
+const submit = () => {
+  getCurentWeather();
+};
 onBeforeMount(() => {
   getCurentWeather();
 });
@@ -99,9 +117,19 @@ onBeforeMount(() => {
     &--btn-toggle {
       margin-left: 10px;
     }
+    @media (max-width: $breakpoint-md-max) {
+      font-size: 50px;
+    }
     @media (max-width: $breakpoint-xs-max) {
       margin: auto;
       font-size: 32px;
+    }
+  }
+  &__input {
+    width: 500px;
+    margin: 20px auto;
+    @media (max-width: $breakpoint-xs-max) {
+  width: 90%;
     }
   }
 }
